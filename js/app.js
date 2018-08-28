5519494595ac19b6d3072973361a1b62c9e59106
saravanan <saravananr6332@gmail.com>
2018-08-28 19:29:59 +0530

    complete the code

--- js/app.js
+++ js/app.js
@@ -1,7 +1,168 @@
-/*
- * Create a list that holds all of your cards
- */
 
+const icons = ["fa fa-diamond", "fa fa-paper-plane-o", "fa fa-paper-plane-o", "fa fa-anchor", "fa fa-diamond", "fa fa-anchor", "fa fa-bolt", "fa fa-bolt", "fa fa-cube", "fa fa-cube", "fa fa-leaf", "fa fa-leaf", "fa fa-bicycle", "fa fa-bicycle", "fa fa-bomb", "fa fa-bomb"];
+
+const cardsContainer = document.querySelector(".deck");
+
+let openedCards = [];
+let matchedCards = [];
+
+
+function init() {
+    for(let i = 0; i < icons.length; i++) {
+        const card = document.createElement("li");
+        card.classList.add("card");
+        card.innerHTML = `<i class="${icons[i]}"></i>`;
+        cardsContainer.appendChild(card);
+    
+        click(card);
+    }
+}
+
+
+
+
+let isFirstClick = true;
+
+function click(card) {
+
+    card.addEventListener("click", function() {
+
+
+        if(isFirstClick) {
+            startTimer();
+            isFirstClick = false;
+        }
+        
+        const currentCard = this;
+        const previousCard = openedCards[0];
+
+        if(openedCards.length === 1) {
+
+            card.classList.add("open", "show", "disable");
+            openedCards.push(this);
+
+            compare(currentCard, previousCard);
+
+        } else {
+            currentCard.classList.add("open", "show", "disable");
+            openedCards.push(this);
+        }
+        
+    });
+}
+
+
+
+function compare(currentCard, previousCard) {
+
+   
+    if(currentCard.innerHTML === previousCard.innerHTML) {
+                
+        
+        currentCard.classList.add("match");
+        previousCard.classList.add("match");
+
+        matchedCards.push(currentCard, previousCard);
+
+        openedCards = [];
+
+        isOver();
+
+    } else {
+        
+        setTimeout(function() {
+            currentCard.classList.remove("open", "show", "disable");
+            previousCard.classList.remove("open", "show", "disable");
+            
+        }, 500);
+
+        openedCards = [];
+        
+    }
+        addMove();
+}
+
+function isOver() {
+    if(matchedCards.length === icons.length) {
+        stopTimer();
+        alert("Hey!, You completed this game");
+        
+    }
+}
+
+const movesContainer = document.querySelector(".moves");
+let moves = 0;
+movesContainer.innerHTML = 0;
+function addMove() {
+    moves++;
+    movesContainer.innerHTML = moves;
+    rating();
+}
+
+
+const starsContainer = document.querySelector(".stars");
+const star = `<li><i class="fa fa-star"></i></li>`;
+starsContainer.innerHTML = star + star + star;
+function rating() {
+
+    if( moves < 10) {
+        starsContainer.innerHTML = star + star + star;
+    } else if( moves < 15) {
+        starsContainer.innerHTML = star + star;
+    } else {
+        starsContainer.innerHTML = star;
+    }
+}
+
+
+const timerContainer = document.querySelector(".timer");
+let liveTimer,
+    totalSeconds = 0;
+
+timerContainer.innerHTML = totalSeconds + 's';
+
+
+ function startTimer() {
+    liveTimer = setInterval(function() {
+        totalSeconds++;
+        timerContainer.innerHTML = totalSeconds + 'seconds';
+    }, 1000);
+}
+
+
+function stopTimer() {
+    clearInterval(liveTimer);
+}
+
+
+
+const restartBtn = document.querySelector(".restart");
+restartBtn.addEventListener("click", function() {
+    cardsContainer.innerHTML = "";
+    init();
+    reset();
+
+});
+
+
+
+function reset() {
+    matchedCards = [];
+
+    moves = 0;
+    movesContainer.innerHTML = moves;
+
+    starsContainer.innerHTML = star + star + star;
+
+
+    stopTimer();
+    isFirstClick = true;
+    totalSeconds = 0;
+    timerContainer.innerHTML = totalSeconds + "s";
+}
+
+
+init();
 
 /*
  * Display the cards on the page
